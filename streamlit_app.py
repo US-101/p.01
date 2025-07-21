d4ccd74b19d8c34939fc156f1bed6524cc0deb71
US-101 <theinsidesign@gmail.com>
2025-07-22 04:45:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 
-name_list = ["小猴", "太郎", "汪太郎"]
+dog_list = ["汪太郎", "莉莉", "狗"]
 
 st.write("歡迎")
-
-for name in name_list:
-    st.write(name + "說你好")
-
+st.write("我是這裡的飼養員")
 name = st.text_input("請問你叫什麼呢？")
-if name:
-    if name == "汪太郎":
-        st.success("汪汪你好")
-    else:
-        st.warning("這裡只歡迎狗")
+if name in dog_list:
+    st.success("汪汪你好")
+else:
+    st.warning("這裡只歡迎狗")
