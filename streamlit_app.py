3cdf67715686c358286afd3603fda6d70415ed19
US-101 <theinsidesign@gmail.com>
2025-07-22 04:28:27 +0900

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+name_list = ["小猴", "太郎", "汪太郎"]
+
+st.write("歡迎")
+
+for name in name_list:
+    if name == "汪太郎":
+        st.write(name + "：汪汪你好")
+    else:
+        st.write(name + "：這裡只歡迎狗")
+
+name = st.text_input("請問你叫什麼呢？")
+if name:
+    if name == "汪太郎":
+        st.success("汪汪你好")
+    else:
+        st.warning("這裡只歡迎狗")
